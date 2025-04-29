05b424392a6e06de1c9e65b87d7481e248493c76
Kristóf Bederna <bederna.kristof@gmail.com>
2025-04-29 18:50:22 +0200

    Balance changes

--- src/main/java/Game/Misc/PlayerStats.java
+++ src/main/java/Game/Misc/PlayerStats.java
@@ -4,30 +4,30 @@ public class PlayerStats {
     public static String currentSave = "";
 
     public static double health = 100;
-    public static double meleeDamage = 53;
+    public static double meleeDamage = 42;
     public static double rangedDamage = 34;
     public static double meleeResistance = 0;
     public static double rangedResistance = 0;
-    public static int gold = 1000;
+    public static int gold = 250;
     public static int meleeKills = 0;
     public static int rangedKills = 0;
     public static int totalMeleeKills = 0;
     public static int totalRangedKills = 0;
-    public static long meleeCooldown = 2000;
-    public static long rangedCooldown = 3000;
+    public static long meleeCooldown = 1750;
+    public static long rangedCooldown = 2500;
 
     public static final double BASELINE_Health = 100;
-    public static final double BASELINE_MeleeDamage = 53;
+    public static final double BASELINE_MeleeDamage = 42;
     public static final double BASELINE_RangedDamage = 34;
     public static final double BASELINE_MeleeResistance = 0;
     public static final double BASELINE_RangedResistance = 0;
-    public static final int BASELINE_Gold = 1000;
+    public static final int BASELINE_Gold = 250;
     public static final int BASELINE_MeleeKills = 0;
     public static final int BASELINE_RangedKills = 0;
     public static final int BASELINE_TotalMeleeKills = 0;
     public static final int BASELINE_TotalRangedKills = 0;
-    public static final long BASELINE_MeleeCooldown = 2000;
-    public static final long BASELINE_RangedCooldown = 3000;
+    public static final long BASELINE_MeleeCooldown = 1750;
+    public static final long BASELINE_RangedCooldown = 2500;
 
     public static void resetToBaseline() {
         health = BASELINE_Health;
--- src/main/java/Game/Misc/ShopItemPrices.java
+++ src/main/java/Game/Misc/ShopItemPrices.java
@@ -1,21 +1,21 @@
 package Game.Misc;
 
 public class ShopItemPrices {
-    public static int Health = 100;
+    public static int Health = 75;
     public static int MeleeDamage = 100;
     public static int RangedDamage = 100;
-    public static int MeleeSpeed = 100;
-    public static int RangedSpeed = 100;
-    public static int MeleeResistance = 100;
-    public static int RangedResistance = 100;
+    public static int MeleeSpeed = 150;
+    public static int RangedSpeed = 175;
+    public static int MeleeResistance = 125;
+    public static int RangedResistance = 125;
 
-    public static final int BASELINE_Health = 100;
+    public static final int BASELINE_Health = 75;
     public static final int BASELINE_MeleeDamage = 100;
     public static final int BASELINE_RangedDamage = 100;
-    public static final int BASELINE_MeleeSpeed = 100;
-    public static final int BASELINE_RangedSpeed = 100;
-    public static final int BASELINE_MeleeResistance = 100;
-    public static final int BASELINE_RangedResistance = 100;
+    public static final int BASELINE_MeleeSpeed = 150;
+    public static final int BASELINE_RangedSpeed = 175;
+    public static final int BASELINE_MeleeResistance = 125;
+    public static final int BASELINE_RangedResistance = 125;
 
     public static void resetToBaseline() {
         Health = BASELINE_Health;
